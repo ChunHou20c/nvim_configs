355125dd6dcd2ce6ddfafc209b3a6c05c544f130
chunhou20c <youmustbekiddingmeshit@gmail.com>
2022-09-17 13:16:35 +0800

    Add toggleterm plugin

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -3,7 +3,7 @@ local opts = {noremap = true, silent = true}
 
 vim.g.mapleader = ' '
 map('n', '<Leader>l', ':NvimTreeToggle<CR>', opts)
-map('n', '<Leader>t', '<C-w>s:resize 10<CR>:ter<CR>a',opts)
+--map('n', '<Leader>t', '<C-w>s:resize 10<CR>:ter<CR>a',opts)
 map('n', '<Leader>r', ':e %<CR>',opts)
 map('n', '<C-Up>', '<C-w>k',opts)
 map('n', '<C-down>', '<C-w>j',opts)
--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -8,5 +8,6 @@ return require'packer'.startup(function()
     use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
     use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
     use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    use 'akinsho/toggleterm.nvim'
     use 'lewis6991/gitsigns.nvim' -- git integration
 end)
